ed989444b527ea3ed1a1d8cf31c2af4366e29274
CrusadeTX <angel.terziev97@gmail.com>
2020-09-03 09:38:31 +0300

    Added Edit View for ToDoNote entities

--- ToDo.API/Startup.cs
+++ ToDo.API/Startup.cs
@@ -27,13 +27,12 @@ namespace ToDo.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews()
-    .AddNewtonsoftJson(options =>
-    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
-);
+           // services.AddControllers()
+    
             services.AddDbContext<AppDbContext>(options => options.
             UseSqlServer(Configuration.GetConnectionString("DbConnection")));
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(options =>
+            options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore); ;
             services.AddScoped<IUserRepository,UserRepository>();
             services.AddScoped<IToDoNoteRepository, ToDoNoteRepository>();
         }
--- ToDo.Web/Pages/EditToDoNoteDetailsBase.cs
+++ ToDo.Web/Pages/EditToDoNoteDetailsBase.cs
@@ -1,11 +1,35 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ToDo.Models;
+using ToDo.Web.Services;
 
 namespace ToDo.Web.Pages
 {
-    public class EditToDoNoteDetailsBase
+    public class EditToDoNoteDetailsBase : ComponentBase
     {
+        public ToDoNote ToDoNote { get; set; } = new ToDoNote();
+        public List<User> Users { get; set; } = new List<User>();
+
+        [Inject]
+        public IUserService UserService { get; set; }
+        [Inject]
+        public IToDoNoteService ToDoNoteService { get; set; }
+        public string UserId { get; set; }
+
+        [Parameter]
+        public string Id { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Id = Id ?? "1";
+            ToDoNote = await ToDoNoteService.GetToDo(int.Parse(Id));
+            Users = (await UserService.GetUsers()).ToList();
+            UserId = ToDoNote.UserId.ToString();
+
+
+        }
     }
 }
